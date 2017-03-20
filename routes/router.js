0fe48b5c081b1d897fc506fb8dbb3a740739bc62
Justin <jchowchow@gmail.com>
2017-03-19 22:06:48 -0700

    baby steps

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require('express'),
       router = express.Router();
 
 router.get('/', (req,res) => {
-  res.render('.pages/index');
+  res.render('./pages/index');
 })
 
 module.exports = router;
